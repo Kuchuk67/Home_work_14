5ab6604220c559b94084583239c1362707a5edc8
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2024-10-22 14:20:14 +0300

    add test

--- src/product.py
+++ src/product.py
@@ -3,13 +3,17 @@ from decimal import Decimal
 class Product:
     name: str
     description: str
-    price: Decimal()
+    price: float
     quantity: int
 
 
-    def __init__(self, name, description, price=0, quantity=0):
+    def __init__(self, name, description, price=0.0, quantity=0):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
+
+x = Product('товар_1','Описание: товар 1', 124.27, 10)
+
+print(x.price/3)
---
+++ tests/conftest.ry.py
@@ -0,0 +1,4 @@
+import pytest
+
+@pytest.fixture
+def Product(name, description: str ,  price , quantity)
